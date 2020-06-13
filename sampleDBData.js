9ab26fe84d89d32975b7a82b967d4d591b95c81d
Magnus <magnus.e@outlook.com>
2020-06-14 00:47:40 +0200

    Updated sample db script

--- sampleDBData.js
+++ sampleDBData.js
@@ -1,78 +1,47 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const Product = require('./src/schemas/db/product');
+const Product = require('./src/schemas/db/productSchema');
 
 mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
 
-// TODO: Make error handler (not that necessary)
-
-db.once('open', function () {
+db.once('open', () => {
   console.log('Connection successful!');
 
-  /*const fakeProducts = [
-    {
-      productName: 'Very nice product',
-      description: 'You won\'t regret buying this',
-      quantityType: 'Things',
-      averageRating: 5,
-      reviewCount: 999,
-      category: 'Great things',
-      price: 1.99,
-      brandName: 'Supreme',
-      brandLogoLink: 'no',
-      discount: 0,
-      discountedPrice: Math.random()
-    },
-    {
-      productName: 'Not a great product',
-      description: 'Buy this at your own risk',
-      quantityType: 'regrets',
-      averageRating: 1.2,
-      reviewCount: 12,
-      category: 'Bad things',
-      price: 13.99,
-      brandName: 'Gccui',
-      brandLogoLink: 'fake',
-      discount: 12.5,
-      discountedPrice: Math.random()
-    }
-  ];*/
-
-  // TODO: Make a bigger variety of fake products
-
-  for (let i = 0; i < 10000; i++) {
-    let product = new Product.model({
-      productName: 'Good product',
-      description: 'It is very good indeed',
-      productImageLinks: ['http://google.no', 'https://bing.com'],
+  for (let i = 0; i < 500; i++) {
+    const product = new Product({
+      name: 'Product',
+      description: 'The only product we sell currently',
+      productImageLinks: [
+        'http://google.no/image.png',
+        'https://bing.com/image.png'
+      ],
       quantityType: 'things',
       averageRating: 4.9,
       reviewCount: 999,
       category: 'Good products',
       price: 1.99,
       brandName: 'Supreme',
-      brandLogoLink: 'https://supreme.com',
+      brandLogoLink:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/576px-Supreme_Logo.svg.png',
       discounts: [
         {
-          discountName: 'Discount for nice children',
-          discountType: 'percentage',
+          name: '90% discount',
+          type: 'percentage',
           value: 90
         },
         {
-          discountName: 'Discount for bad children',
-          discountType: 'percentage',
+          name: '2% discount',
+          type: 'percentage',
           value: 2
         }
       ]
     });
 
-    Product.model.collection.insertOne(product, function (err, docs) {
-      if (err) {
-        return console.log(err);
-      }
+    Product.collection.insertOne(product, (err, docs) => {
+      if (err) return console.error(err);
       console.log(docs);
     });
   }

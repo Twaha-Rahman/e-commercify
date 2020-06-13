2b8742c45702e520b86ec04dcfdfdfd7b659dd69
Twaha-Rahman <mahi6703890@gmail.com>
2020-06-13 18:23:07 +0600

    Added BannerType

---
+++ src/schemas/graphql/BannerType.js
@@ -0,0 +1,17 @@
+const graphQL = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const ProductType = new GraphQLObjectType({
+  name: 'Banner',
+  fields: () => {
+    return {
+      bannerImageLink: { type: GraphQLString },
+      bannerGoToLink: { type: GraphQLString },
+      text: { type: GraphQLString },
+      CTA: { type: GraphQLString }
+    };
+  }
+});
+
+module.exports = ProductType;

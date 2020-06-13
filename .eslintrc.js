462c0f9af8ecbcf1f471f4c8d0791ed031f57cf8
snowteamer <64228468+snowteamer@users.noreply.github.com>
2020-06-14 01:46:28 +0200

    Make the linting stricter
    How: by enabling the recommended ESLint preset.
    
    There should be no conflict with Prettier since we also extend from
    'plugin:prettier/recommended' **later**.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: 'plugin:prettier/recommended',
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'

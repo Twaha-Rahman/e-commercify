67b573ef3233e5faacbb566e8844e85ae7ee2c17
Magnus <magnus.e@outlook.com>
2020-06-14 14:10:41 +0200

    Made "price" required

--- src/schemas/db/productSchema.js
+++ src/schemas/db/productSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const discountSchema = require('./discountSchema');
 
-const URLValidationRegex =
+const urlValidationRegex =
   '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})';
 
 const productSchema = new mongoose.Schema({
@@ -14,7 +14,7 @@ const productSchema = new mongoose.Schema({
   productImageLinks: [
     {
       type: String,
-      match: URLValidationRegex,
+      match: urlValidationRegex,
       required: true
     }
   ],
@@ -30,11 +30,14 @@ const productSchema = new mongoose.Schema({
   },
   reviewCount: Number,
   category: String,
-  price: Number,
+  price: {
+    type: Number,
+    required: true
+  },
   brandName: String,
   brandLogoLink: {
     type: String,
-    match: URLValidationRegex
+    match: urlValidationRegex
   },
   discounts: [discountSchema]
 });

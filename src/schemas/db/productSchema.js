5f299580998ca7f1a3d1d575f8c83077f4b06240
Twaha-Rahman <mahi6703890@gmail.com>
2020-06-14 21:07:09 +0600

    Fixed the linting issue with the long line

--- src/schemas/db/productSchema.js
+++ src/schemas/db/productSchema.js
@@ -3,8 +3,13 @@
 const mongoose = require('mongoose');
 const discountSchema = require('./discountSchema');
 
-const urlValidationRegex =
-  '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})';
+const partsOfRegex = [
+  '(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]',
+  '+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}',
+  '|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})'
+];
+
+const urlValidationRegex = partsOfRegex.join('');
 
 const productSchema = new mongoose.Schema({
   productId: mongoose.Schema.Types.ObjectID,

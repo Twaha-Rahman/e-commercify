b63b7771f5227aee9b92905931f1119b5366b4a3
snowteamer <64228468+snowteamer@users.noreply.github.com>
2020-06-16 00:01:03 +0200

    Optimize sample data injector script

--- sampleDBData.js
+++ sampleDBData.js
@@ -1,54 +1,98 @@
-'use strict';
-
 /**
  * @file Injects sample product data into MongoDB
  */
+'use strict';
 
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 const Product = require('./src/models/db/product');
 
-mongoose.connect(process.env.MONGO_URL);
+if (typeof process.env.MONGO_URL !== 'string') {
+  console.error('Error: the MONGO_URL environment variable is not set.');
+  process.exit(1);
+}
 
-const db = mongoose.connection;
+const chooseRandomInteger = (min, max) => {
+  return min + Math.floor(Math.random() * max);
+};
 
-db.once('open', () => {
-  console.log('Connection successful!');
+const chooseRandomPercentage = () => {
+  return chooseRandomInteger(1, 99) / 2;
+};
 
-  for (let i = 0; i < 500; i++) {
-    const product = new Product({
-      name: 'Product',
-      description: 'The only product we sell currently',
-      productImageLinks: [
-        'http://google.no/image.png',
-        'https://bing.com/image.png'
-      ],
-      quantityType: 'things',
-      averageRating: 4.9,
-      reviewCount: 999,
-      category: 'Good products',
-      price: 1.99,
-      brandName: 'Supreme',
-      brandLogoLink:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/576px-Supreme_Logo.svg.png',
-      discounts: [
-        {
-          name: '90% discount',
-          type: 'percentage',
-          value: 90
-        },
-        {
-          name: '2% discount',
-          type: 'percentage',
-          value: 2
-        }
-      ]
-    });
+const chooseRandomProductId = () => {
+  return Math.random().toString(16).slice(2, 8).toUpperCase();
+};
 
-    Product.collection.insertOne(product, (err, docs) => {
-      if (err) return console.error(err);
-      return console.log(docs);
-    });
-  }
-});
+const chooseRandomPrice = () => {
+  return chooseRandomInteger(1, 9999) / 100;
+};
+
+const chooseRandomRating = () => {
+  return chooseRandomInteger(0, 10) / 2;
+};
+
+const createRandomProduct = () => {
+  return new Product({
+    name: 'Product',
+    description: 'The only product we sell currently',
+    productImageLinks: [
+      'http://google.no/image.png',
+      'https://bing.com/image.png'
+    ],
+    quantityType: 'things',
+    averageRating: chooseRandomRating(),
+    reviewCount: chooseRandomInteger(0, 999),
+    category: 'Good products',
+    price: chooseRandomPrice(),
+    brandName: 'Supreme',
+    brandLogoLink:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/576px-Supreme_Logo.svg.png',
+    discounts: [
+      {
+        name: '90% discount',
+        type: 'percentage',
+        value: 90
+      },
+      {
+        name: '2% discount',
+        type: 'percentage',
+        value: 2
+      }
+    ]
+  });
+};
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then((client) => {
+    console.log('Connection successful!');
+  })
+  .catch((error) => {
+    console.error('Could not connect to MongoDB!', error);
+    process.exit(1);
+  })
+  .then(() => {
+    const products = [];
+
+    for (let i = 0; i < 500; i++) {
+      products[i] = createRandomProduct();
+    }
+    return products;
+  })
+  .then((products) => {
+    return Product.insertMany(products);
+  })
+  .then((docs) => {
+    console.log('Success: all %s objects were inserted.', docs.length);
+  })
+  .catch((error) => {
+    console.error('Could not insert all objects into the database', error);
+    process.exitCode = 1;
+  })
+  .then(() => {
+    mongoose.connection.close();
+  });

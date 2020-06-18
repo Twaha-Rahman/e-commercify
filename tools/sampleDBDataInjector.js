a0c11358b0e5d91cf5aaaa11e6e79ff5cbd391f2
Twaha-Rahman <mahi6703890@gmail.com>
2020-06-18 16:26:57 +0600

    Further refactor sampleDBDataInjector

--- tools/sampleDBDataInjector.js
+++ tools/sampleDBDataInjector.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const Product = require('../src/models/db/product');
+const sampleProductData = require('../sample-data/sampleProductData.json');
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -22,34 +23,7 @@ db.once('open', async () => {
   let errorObject;
 
   for (let i = 0; i < process.env.AMOUNT_OF_SAMPLE_PRODUCT_DATA; i++) {
-    const product = new Product({
-      productName: 'Product',
-      description: 'The only product we sell currently',
-      productImageLinks: [
-        'http://google.no/image.png',
-        'https://bing.com/image.png'
-      ],
-      quantityType: 'things',
-      averageRating: 4.9,
-      reviewCount: 999,
-      category: 'Good products',
-      price: 1.99,
-      brandName: 'Supreme',
-      brandLogoLink:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Supreme_Logo.svg/576px-Supreme_Logo.svg.png',
-      discounts: [
-        {
-          discountName: '90% discount',
-          type: 'percentage',
-          value: 90
-        },
-        {
-          discountName: '2% discount',
-          type: 'percentage',
-          value: 2
-        }
-      ]
-    });
+    const product = new Product(sampleProductData);
     try {
       await product.save();
     } catch (error) {

fda0bce49ceeaebae82e9d8a7d5b7b49ee9d43dc
Twaha-Rahman <mahi6703890@gmail.com>
2020-06-18 16:14:49 +0600

    Refactored the code

--- tools/sampleDBDataInjector.js
+++ tools/sampleDBDataInjector.js
@@ -9,14 +9,19 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const Product = require('../src/models/db/product');
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
-db.once('open', () => {
-  console.log('Connection successful!');
+db.once('open', async () => {
+  console.log('\nConnection successful!\n');
 
-  for (let i = 0; i < 500; i++) {
+  let errorObject;
+
+  for (let i = 0; i < process.env.AMOUNT_OF_SAMPLE_PRODUCT_DATA; i++) {
     const product = new Product({
       productName: 'Product',
       description: 'The only product we sell currently',
@@ -45,6 +50,24 @@ db.once('open', () => {
         }
       ]
     });
-    product.save().then(console.log).catch(console.log);
+    try {
+      await product.save();
+    } catch (error) {
+      errorObject = error;
+    }
   }
+
+  if (errorObject) {
+    console.error(
+      'An error occured while trying to inject data into the database!\n\n',
+      errorObject
+    );
+  } else {
+    console.log(
+      `Successfully inserted ${process.env.AMOUNT_OF_SAMPLE_PRODUCT_DATA}` +
+        ` sample product data into the database!`
+    );
+  }
+
+  process.exit();
 });

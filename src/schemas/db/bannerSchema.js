adaedace3424b459e5a2c854c71257124e3b7295
Twaha-Rahman <mahi6703890@gmail.com>
2020-07-10 19:41:27 +0600

    Update banner's schema

--- src/schemas/db/bannerSchema.js
+++ src/schemas/db/bannerSchema.js
@@ -4,6 +4,9 @@ const { Schema } = require('mongoose');
 const { ObjectId } = Schema.Types;
 
 const bannerSchema = new Schema({
+  bannerId: {
+    type: ObjectId
+  },
   dateAdded: {
     type: Number,
     required: true

30d0e08d038d62ced42300e30db6b6e77d208a32
Twaha-Rahman <mahi6703890@gmail.com>
2020-08-01 16:41:54 +0600

    Add Timestamps on the Product model

--- src/schemas/db/productSchema.js
+++ src/schemas/db/productSchema.js
@@ -1,42 +1,45 @@
 const { Schema } = require('mongoose');
 
-const productSchema = new Schema({
-  productId: Schema.Types.ObjectID,
-  name: {
-    type: String,
-    required: true
-  },
-  description: String,
-
-  // Write regex that'll check for the right file extensions for `imageLinks`
-  imageLinks: [
-    {
+const productSchema = new Schema(
+  {
+    productId: Schema.Types.ObjectID,
+    name: {
       type: String,
       required: true
-    }
-  ],
-  quantityType: {
-    type: String,
-    default: 'pieces'
+    },
+    description: String,
+
+    // Write regex that'll check for the right file extensions for `imageLinks`
+    imageLinks: [
+      {
+        type: String,
+        required: true
+      }
+    ],
+    quantityType: {
+      type: String,
+      default: 'pieces'
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5
+    },
+    reviewCount: Number,
+    category: String,
+    price: {
+      type: Number,
+      required: true
+    },
+    brandName: String,
+    brandLogoLink: {
+      type: String
+    },
+    discount: String,
+    discountedPrice: String
   },
-  averageRating: {
-    type: Number,
-    default: 0,
-    min: 0,
-    max: 5
-  },
-  reviewCount: Number,
-  category: String,
-  price: {
-    type: Number,
-    required: true
-  },
-  brandName: String,
-  brandLogoLink: {
-    type: String
-  },
-  discount: String,
-  discountedPrice: String
-});
+  { timestamps: true }
+);
 
 module.exports = productSchema;

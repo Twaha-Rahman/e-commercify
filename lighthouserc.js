80897b9e5c7a5710aa78aca9b23e0d127133e796
Twaha-Rahman <mahi6703890@gmail.com>
2020-08-02 12:03:51 +0600

    Correct the port

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
+      url: ['http://localhost:8080/'],
       startServerCommand:
         'npm run start & cd frontend && npm run serve & sleep 15 && cd ../' // eslint-disable-line
     },

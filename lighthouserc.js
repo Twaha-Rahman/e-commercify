25c4b7a7a12d851437789fe60f0aed84bc2c2a57
Twaha-Rahman <mahi6703890@gmail.com>
2020-08-02 12:31:59 +0600

    Add JSDoc

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,3 +1,9 @@
+/**
+ * @file - Conifiguration for LightHouse CI. Vist the link below
+ * for more details ->
+ * https://github.com/GoogleChrome/lighthouse/blob/master/docs/configuration.md
+ */
+
 module.exports = {
   ci: {
     collect: {

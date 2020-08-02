9f90073d3cda5fc2c7bfbd9561844fbff4d6e254
Twaha Rahman <mahi6703890@gmail.com>
2020-08-02 20:27:47 +0600

    Add basic navbar

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -2,21 +2,36 @@ import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style.css';
 
-const Header = () => (
-  <header class={style.header}>
-    <h1>Preact App</h1>
-    <nav>
+import HomeSvg from '../svg/HomeSvg';
+import BoxSvg from '../svg/BoxSvg';
+import SearchSvg from '../svg/SearchSvg';
+import CartSvg from '../svg/CartSvg';
+import CogSvg from '../svg/CogSvg';
+
+const Header = () => {
+  const color = 'var(--background-color-dark)';
+
+  return (
+    <nav class={style.header}>
       <Link activeClassName={style.active} href="/">
-        Home
+        <HomeSvg color={color} />
       </Link>
       <Link activeClassName={style.active} href="/profile">
-        Me
+        <BoxSvg color={color} />
       </Link>
       <Link activeClassName={style.active} href="/profile/john">
-        John
+        <SearchSvg color={color} />
+      </Link>
+
+      <Link activeClassName={style.active} href="/profile/john">
+        <CartSvg color={color} />
+      </Link>
+
+      <Link activeClassName={style.active} href="/profile/john">
+        <CogSvg color={color} />
       </Link>
     </nav>
-  </header>
-);
+  );
+};
 
 export default Header;

596c4e22c3a839262b38e63d4d527cd499fafe94
Twaha-Rahman <mahi6703890@gmail.com>
2020-08-03 09:59:50 +0600

    Add carousel component skeliton

---
+++ frontend/src/components/Carousel/index.js
@@ -0,0 +1,9 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match'; // eslint-disable-line
+import style from './style.css'; // eslint-disable-line
+
+const Header = () => {
+  return <div>Test</div>;
+};
+
+export default Header;

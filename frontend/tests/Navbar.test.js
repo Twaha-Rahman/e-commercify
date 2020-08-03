08341c1e21cf34dfe1e3bbf234af071b66e0437f
Twaha Rahman <mahi6703890@gmail.com>
2020-08-03 11:08:58 +0600

    Update Navbar test

--- frontend/tests/Navbar.test.js
+++ frontend/tests/Navbar.test.js
@@ -1,11 +1,11 @@
 import { h } from 'preact';
-import Header from '../src/components/header';
+import Navbar from '../src/components/Navbar';
 // See: https://github.com/preactjs/enzyme-adapter-preact-pure
 import { shallow } from 'enzyme';
 
-describe('Initial Test of the Header', () => {
+describe('Initial Test of the Navbar', () => {
   test('Navbar renders 5 items', () => {
-    const context = shallow(<Header />);
+    const context = shallow(<Navbar />);
     expect(context.find('Link').length).toBe(5);
   });
 });

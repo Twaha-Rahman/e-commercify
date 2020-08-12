192c60b9a336b75649cf82002af8faed772b6e32
Twaha Rahman <mahi6703890@gmail.com>
2020-08-12 14:10:43 +0600

    Minimal carousel

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -3,28 +3,53 @@ import { useState, useEffect } from 'preact/hooks'; // eslint-disable-line
 import { Link } from 'preact-router/match'; // eslint-disable-line
 import style from './style.css'; // eslint-disable-line
 
+import Siema from 'siema';
+
 import Kiwi from '../../assets/product-placeholder/Kiwi-Slice-PNG-File.png';
 import Mango from '../../assets/product-placeholder/Mango-PNG-Image-Free-Download.png'; // eslint-disable-line
 import Banner from '../../assets/product-placeholder/banner.png'; // eslint-disable-line
 
 const images = [Kiwi, Mango]; // eslint-disable-line
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- */
-// function getRandomInt(min, max) {
-//   const newMin = Math.ceil(min);
-//   const newMax = Math.floor(max);
-//   return Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
-// }
-// const randomIndex = getRandomInt(0, 1);
-
 const Header = () => {
-  return <>{/* Plonk in Siema here */}</>;
+  useEffect(() => {
+    new Siema();
+  });
+
+  return (
+    <>
+      <div class="siema">
+        <div>
+          <img
+            class="siema-slide"
+            src="https://pawelgrzybek.github.io/siema/assets/siema--pink.svg"
+            alt="Siema image"
+          />
+        </div>
+        <div>
+          <img
+            class="siema-slide"
+            src="https://pawelgrzybek.github.io/siema/assets/siema--yellow.svg"
+            alt="Siema image"
+          />
+        </div>
+        <div>
+          <img
+            class="siema-slide"
+            src="https://pawelgrzybek.github.io/siema/assets/siema--pink.svg"
+            alt="Siema image"
+          />
+        </div>
+        <div>
+          <img
+            class="siema-slide"
+            src="https://pawelgrzybek.github.io/siema/assets/siema--yellow.svg"
+            alt="Siema image"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
